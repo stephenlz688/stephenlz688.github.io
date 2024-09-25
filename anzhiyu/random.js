9275e0b1c1a7ff61d969571105248405b52cd4bb
stephenlz688 <1764878592@qq.com>
2024-09-25 23:42:37 +0800

    Site updated: 2024-09-25 23:42:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/23/hello-world/","2024/09/23/markdown简单操作/"];function toRandomPost(){
+var posts=["2024/09/23/markdown简单操作/","2024/09/25/静态成员小练习1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
